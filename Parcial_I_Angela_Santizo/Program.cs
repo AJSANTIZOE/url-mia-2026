53ce19da7919d3ba5801a916b1dbc58c0ac836d2
Angela Santizo0 <ajsantizoe@correo.url.edu.gt>
2026-08-05 08:18:53 -0600

    feat: agrega soluciones del parcial 1 - Angela Santizo

---
+++ Parcial_I_Angela_Santizo/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+
+Console.Write("Ingrese su nombre completo: ");
+string nombre = Console.ReadLine();
+
+Console.Write("Ingrese la ruta del archivo de texto: ");
+string ruta = Console.ReadLine();
+
+if (File.Exists(ruta))
+{
+    string[] lineas = File.ReadAllLines(ruta);
+    string texto = File.ReadAllText(ruta);
+
+    int numeroLineas = lineas.Length;
+    int numeroCaracteres = texto.Length;
+    int numeroVocales = 0;
+
+    for (int i = 0; i < texto.Length; i++)
+    {
+        char letra = Char.ToLower(texto[i]);
+
+        if (letra == 'a' ||
+            letra == 'e' ||
+            letra == 'i' ||
+            letra == 'o' ||
+            letra == 'u')
+        {
+            numeroVocales++;
+        }
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("===== RESULTADOS =====");
+    Console.WriteLine("Líneas: " + numeroLineas);
+    Console.WriteLine("Vocales: " + numeroVocales);
+    Console.WriteLine("Caracteres: " + numeroCaracteres);
+
+    string carpeta = @"C:\MIA_Parcial_1";
+
+    string nombreArchivo = nombre.Replace(" ", "_");
+
+    string rutaCSV = carpeta + @"\resultados_" + nombreArchivo + ".csv";
+
+    StreamWriter archivo = new StreamWriter(rutaCSV);
+
+    archivo.WriteLine("Nombre,Lineas,Vocales,Caracteres");
+    archivo.WriteLine(nombreArchivo + "," +
+                      numeroLineas + "," +
+                      numeroVocales + "," +
+                      numeroCaracteres);
+
+    archivo.Close();
+
+    Console.WriteLine();
+    Console.WriteLine("Archivo CSV creado correctamente.");
+    Console.WriteLine("Ruta: " + rutaCSV);
+}
+else
+{
+    Console.WriteLine("El archivo no existe.");
+}
+
+Console.WriteLine();
+Console.WriteLine("Presione una tecla para finalizar...");
+Console.ReadKey();
